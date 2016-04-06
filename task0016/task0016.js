78ece52391561f984a0679873787a2dd5b3eb3a2
EstellaLi <983793660@qq.com>
2016-04-06 16:10:46 +0800

    tsk16

--- task0016/task0016.js
+++ task0016/task0016.js
@@ -6,7 +6,7 @@
  *    "上海": 40
  * };
  */
-var aqiData = [];
+var aqiData = {};
 /**
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
@@ -17,9 +17,7 @@ function addAqiData() {
 
 	if (!isNaN(aqi) && parseInt(aqi) == parseFloat(aqi)) {
 		if (city.match(/[\u4e00-\u9fa5|a-zA-Z]+/)) {
-			aqiData.push({
-				city, aqi
-			});
+			aqiData[city] = aqi;
 		} else {
 			alert("城市名称应为中英文字符");
 		}
@@ -32,18 +30,14 @@ function addAqiData() {
  * 渲染aqi-table表格
  */
 function renderAqiList() {
-	var table = document.getElementById("aqi-table");
-	table.innerHTML = "<tr><td>城市</td><td>空气质量</td><td>操作</td></tr>";
-	for (var i = 0; i < aqiData.length; i++) {
-		table.innerHTML += "<tr><td>" + aqiData[i].city + "</td><td>" + aqiData[i].aqi + "</td><td><button id='button" + i + "' >删除</button></td></tr>";
-			
-		(function (t){
-			var btn = document.getElementById("button" + t);
-			btn.addEventListener("click", function(){alert(t);}, false);
-
-		})(i);
-		
+	var t = "<tr><td>城市</td><td>空气质量</td><td>操作</td></tr>";
+	var ori = t;
+	for(var city in aqiData){
+		t += "<tr><td>" + city + "</td><td>" + aqiData[city] + "</td><td><button data-city='"+ city +"'>删除</button></td></tr>";
 	}
+	
+	document.getElementById("aqi-table").innerHTML = t == ori? "":t;
+
 }
 
 /**
@@ -59,17 +53,22 @@ function addBtnHandle() {
  * 点击各个删除按钮的时候的处理逻辑
  * 获取哪个城市数据被删，删除数据，更新表格显示
  */
-function delBtnHandle() {
+function delBtnHandle(city) {
 	// do sth.
-	console.log(this);
+	delete aqiData[city];
+	renderAqiList();	
 }
 
 function init() {
-
 	// 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-
-	// 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
 	document.getElementById("add-btn").onclick = addBtnHandle;
+	// 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
+	document.getElementById("aqi-table").addEventListener("click",function(e){
+		//e.target是被点击的元素 ie：e.srcElement
+		var target=e.target || e.srcElement;
+		delBtnHandle(target.dataset.city);
+	});
 }
 
 window.onload = init;
+
