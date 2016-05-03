96fb6e29ef2693d8576747432a2d3b59fa615c20
estellali <983793660@qq.com>
2016-05-03 20:23:18 +0800

    task37

--- task0037/task37.js
+++ task0037/task37.js
@@ -7,10 +7,12 @@ function addListener(element, event, handler) {
 		element["on" + event] = handler;
 	}
 }
-//ctrl+alt+f
 function $(selector) {
 	return document.querySelector(selector);
 }
+/*
+实现相对于浏览器视口自动居中
+*/
 var autoCenter = function() {
 	var element = arguments[0] ? arguments[0] : $(".show");
 	var eleW = element.offsetWidth;
@@ -22,13 +24,50 @@ var autoCenter = function() {
 	element.style.left = (docW - eleW) / 2 + "px";
 	element.style.top = (docH - eleH) / 2 + "px";
 }
+/*
+实现遮光罩全屏
+*/
 var fullMask = function(){
 	var element = arguments[0] ? arguments[0] : $(".mask");
-	element.style.width = 
+	element.style.width = document.documentElement.clientWidth + "px";
+	element.style.height = document.documentElement.clientHeight + "px";
 }
+
+var isDrag = false;
+var mouseRelativeX =0;
+var mouseRelativeY =0;
+
+
+
 window.onload = function() {
 	autoCenter($(".show"));
+	fullMask();
+	addListener($(".mask"), "click", function(){
+		$(".mask").style.display = "none";
+		$(".show").style.display = "none";
+	});
+	addListener($("#btn-show"), "click", function(){
+		$(".mask").style.display = "block";
+		$(".show").style.display = "block";
+		autoCenter($(".show"));
+	});
+	addListener($(".show"),"mousedown", function(){
+		isDrag = true;
+		mouseRelativeX = event.clientX - $(".show").offsetLeft;
+		mouseRelativeY = event.clientY - $(".show").offsetTop;		
+	});
+	addListener($(".mask"), "mousemove", function(){
+		var e = event;
+		if(isDrag){
+			$(".show").style.left = (e.clientX - mouseRelativeX) + "px";
+			$(".show").style.top = (e.clientY - mouseRelativeY) + "px";
+		}
+	});
+	addListener($(".show"), "mouseup", function(){
+		isDrag = false;
+	})
 }
 window.onresize = function() {
 	autoCenter($(".show"));
+	fullMask();
 }
